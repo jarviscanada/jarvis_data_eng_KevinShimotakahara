637212f7ecc72718e8c0f3b1ec8d83f7c078f86e
KevinShimotakahara <kshimotakahara@gmail.com>
2020-09-01 01:58:45 +0000

    finished queries.sql

--- linux_sql/sql/queries.sql
+++ linux_sql/sql/queries.sql
@@ -12,10 +12,14 @@ $$
 
 SELECT host_id, host_name,
        round5(host_usage.timestamp) AS timestamp5min,
-       round(AVG((total_mem*1000 :: float - memory_free :: float)/(total_mem*10))) AS avg_used_mem_percentage
+       round(AVG((total_mem :: float - memory_free*1000 :: float)*100/(total_mem))) AS avg_used_mem_percentage
 	FROM host_usage
 		INNER JOIN host_info
 		ON host_usage.host_id = host_info.id
     GROUP BY host_id, host_name, timestamp5min, total_mem, memory_free
 	ORDER BY host_id;
 
+SELECT host_id, ts, COUNT(*) AS num_data_points
+	FROM (SELECT host_id, round5(host_usage.timestamp) AS ts FROM host_usage) AS alias
+    GROUP BY ts, alias.host_id
+    ORDER BY host_id;
