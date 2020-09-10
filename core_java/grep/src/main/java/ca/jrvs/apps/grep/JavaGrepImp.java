2a00779845a684b71014b623edc8117f11436e76
KevinShimotakahara <kshimotakahara@gmail.com>
2020-09-10 04:41:54 +0000

    95% done grep Design and Implementation ticket

---
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
@@ -0,0 +1,166 @@
+package ca.jrvs.apps.grep;
+
+//import com.sun.org.slf4j.internal.Logger;
+//import com.sun.org.slf4j.internal.LoggerFactory;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class JavaGrepImp implements JavaGrep {
+
+  //final Logger logger = LoggerFactory.getLogger(JavaGrep.class);
+
+  private String regex;
+  private String rootPath;
+  private String outFile;
+
+  /**
+   * Top level search workflow
+   * @throws IOException
+   */
+  public void process() throws IOException {
+    ArrayList<String> matchedLines = new ArrayList<String>();
+    for (File file : listFiles(rootPath)){
+      for (String line : readLines(file)){
+        if (containsPattern(line)){
+          matchedLines.add(line);
+        }
+      }
+    }
+    writeToFile(matchedLines);
+  }
+
+  /**
+   * Traverse a given directory and return all files
+   * @param rootDir input directory
+   * @return files under the rootDir
+   */
+  public List<File> listFiles(String rootDir){
+    File dir = new File(rootDir);
+    File[] files = dir.listFiles();
+    List<File> filesFound = new ArrayList<File>();
+    if (files != null && files.length > 0){
+      for (File file : files){
+        // Check if the file is a directory
+        if (file.isDirectory()){
+          //recursively call listFiles
+          filesFound.addAll(listFiles(file.getAbsolutePath()));
+        }else{
+          filesFound.add(file);
+        }
+      }
+    }
+    return filesFound;
+  }
+
+  /**
+   * Read a file and return all the lines
+   *
+   * Explain FileReader, BufferedReader, and character encoding
+   *
+   * @param inputFile file to be read
+   * @return lines
+   * @throws IllegalArgumentException if a given inputFile is not a file
+   * @throws FileNotFoundException
+   */
+  public List<String> readLines(File inputFile)
+      throws IllegalArgumentException, FileNotFoundException {
+    Scanner myReader = new Scanner(inputFile);
+    List<String> fileLines = new ArrayList<String>();
+    while (myReader.hasNextLine()) {
+      String data = myReader.nextLine();
+      fileLines.add(data);
+    }
+
+    return fileLines;
+  }
+
+  /**
+   * check if a line contains the regex patter (passed by user)
+   * @param line input string
+   * @return true if there is a match
+   */
+  public boolean containsPattern(String line) {
+    Pattern pattern = Pattern.compile(regex);
+    Matcher matcher = pattern.matcher(line);
+    return matcher.find();
+  }
+
+  /**
+   * Write lines to a file
+   *
+   * Explore: FileOutputStream, OutputStreamWriter, and BufferedWriter
+   *
+   * @param lines matched line
+   * @throws IOException if write failed
+   */
+  public void writeToFile(List<String> lines) throws IOException {
+    FileWriter myWriter = new FileWriter(outFile);
+    for(String line : lines){
+      myWriter.write(line);
+      myWriter.write("\n");
+    }
+    myWriter.close();
+  }
+
+  public String getRootPath(){
+    return this.rootPath;
+  }
+
+  public void setRootPath(String rootPath){
+    this.rootPath = rootPath;
+  }
+
+  public String getRegex(){
+    return this.regex;
+  }
+
+  public void setRegex(String regex){
+    this.regex = regex;
+  }
+
+  public String getOutFile(){
+    return this.outFile;
+  }
+
+  public void setOutFile(String outFile){
+    this.outFile = outFile;
+  }
+
+  public static void main(String[] args) throws FileNotFoundException {
+    //if (args.length != 3){
+    //  throw new IllegalArgumentException("USAGE: JavaGrep regex rootPath outFile");
+    //}
+
+    //Use default logger config
+    //BasicConfigurator.configure(); //no idea what this is and it is making the IDE mad
+
+    JavaGrepImp javaGrepImp = new JavaGrepImp();
+    //javaGrepImp.setRegex(args[0]);
+    //javaGrepImp.setRootPath(args[1]);
+    //javaGrepImp.setOutFile(args[2]);
+
+    //testing
+    javaGrepImp.setRegex(".*Romeo.*Juliet.*");
+    javaGrepImp.setOutFile("//home//centos//dev//jarvis_data_eng_kevin//core_java//grep//out//grep_2020-09-10_04:22:06.txt");
+    javaGrepImp.setRootPath("//home//centos//dev//jarvis_data_eng_kevin//core_java//grep//data");
+    //List<File> test = javaGrepImp.listFiles(javaGrepImp.getRootPath());
+    //List<String> otherTest = javaGrepImp.readLines(test.get(0));
+    //end testing
+
+    try {
+      javaGrepImp.process();
+    } catch (Exception ex) {
+      //javaGrepImp.logger.error(ex.getMessage(), ex);
+      int dasd = 1;
+    }
+
+  }
+}
